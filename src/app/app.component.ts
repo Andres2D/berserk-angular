4cd1aa7dad30328aa767d5dfd109cfa07eeeffc7
Andres2D <andres2d1997.1206@gmail.com>
2022-08-24 19:37:44 -0500

    Add big input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'async-input';
+  bigInput: FormControl = new FormControl('');
+  
+  ngOnInit(): void {
+    this.bigInput.valueChanges.subscribe((value) => {
+      console.log(value);
+    });  
+  }
 }
