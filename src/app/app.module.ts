a8f5cc8c842a86012a9fc528b2e46b7a5f1c5bc9
Andres2D <andres2d1997.1206@gmail.com>
2022-08-26 21:36:32 -0500

    Add fake page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FakePageComponent } from './pages/fake-page/fake-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FakePageComponent
   ],
   imports: [
     BrowserModule
