9eacbef1816d1068775ad46933bd6f972a58778b
Andres2D <andres2d1997.1206@gmail.com>
2023-02-26 19:58:02 -0500

    Use page button from paginator comonent

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.scss']
 })
-export class PaginatorComponent {
+export class PaginatorComponent implements OnInit {
+  @Input() pages: number = 7;
+  @Input() currentPage: number = 2;
 
+  fullPagesList: number[] = [];
+
+  ngOnInit(): void {
+    this.fullPagesList = Array.from({length: this.pages}).map((_, i) => i+1);
+  }
 }
