ab359b599d5099dde5622d616d28bb01df3cf7d3
Andres2D <andres2d1997.1206@gmail.com>
2023-04-05 19:53:44 -0500

    Add bad code mini app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'async-input';
+
+  response: FormControl = new FormControl('');
+
+  constructor(private readonly http: HttpClient) {}
+
+  ask(actionKey: string): void {
+    if(actionKey === 'joke') {
+      this.http.get('https://dad-jokes.p.rapidapi.com/random/joke', {
+        headers: {
+          'X-RapidAPI-Key': 'your_api_key',
+          'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+        }
+      }).subscribe((res: any) => {
+        this.response.setValue(`${res.body[0].setup}... \n\n${res.body[0].punchline}`);
+      });
+    }else if(actionKey === 'quote'){
+      this.http.get('https://famous-quotes4.p.rapidapi.com/random?category=all&count=1', {
+        headers: {
+          'X-RapidAPI-Key': 'your_api_key',
+          'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com'
+        }
+      }).subscribe((res: any) => {
+        this.response.setValue(res[0].text);
+      });
+    }else if(actionKey === 'wordle'){
+      this.http.get('https://wordle-answers-solutions.p.rapidapi.com/today', {
+        headers: {
+          'X-RapidAPI-Key': 'your_api_key',
+          'X-RapidAPI-Host': 'wordle-answers-solutions.p.rapidapi.com'
+        }
+      }).subscribe((res: any) => {
+        this.response.setValue(res.today);
+      });
+    }else if(actionKey === 'should'){
+      const options = ['Yes, do it.', `Please don't do it.`];
+      this.response.setValue(options[Math.floor(Math.random() * 2)]);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
